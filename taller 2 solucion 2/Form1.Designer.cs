ae16759ac683ac03f89dc98188ecc7043ebadcf8
yhonatan alexis lopez salazar <yhonatanalexislopezsalazar@gmail.com>
2020-08-21 12:48:47 -0500

    se realisa programa de subir notas y desirte si pasastes o no

---
+++ taller 2 solucion 2/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace taller_2_solucion_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.txtNota5 = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.lblpromedio = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblmensaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label1.Location = new System.Drawing.Point(324, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "promedio de notas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(137, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ingrese la primera nota";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(137, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ingrese segunda nota";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label4.Location = new System.Drawing.Point(137, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ingrese la tercera nota";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label5.Location = new System.Drawing.Point(137, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ingrese la cuarta nota";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label6.Location = new System.Drawing.Point(137, 279);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(169, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "ingrese la quinta nota";
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(328, 74);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 20);
+            this.txtNota1.TabIndex = 6;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(328, 185);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 20);
+            this.txtNota3.TabIndex = 7;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(328, 137);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 20);
+            this.txtNota2.TabIndex = 8;
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.Location = new System.Drawing.Point(328, 235);
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(100, 20);
+            this.txtNota4.TabIndex = 9;
+            // 
+            // txtNota5
+            // 
+            this.txtNota5.Location = new System.Drawing.Point(328, 279);
+            this.txtNota5.Name = "txtNota5";
+            this.txtNota5.Size = new System.Drawing.Size(100, 20);
+            this.txtNota5.TabIndex = 10;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.btncalcular.Location = new System.Drawing.Point(540, 375);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(119, 49);
+            this.btncalcular.TabIndex = 11;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.btnlimpiar.Location = new System.Drawing.Point(255, 375);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(108, 49);
+            this.btnlimpiar.TabIndex = 12;
+            this.btnlimpiar.Text = "limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // lblpromedio
+            // 
+            this.lblpromedio.AutoSize = true;
+            this.lblpromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpromedio.Location = new System.Drawing.Point(591, 137);
+            this.lblpromedio.Name = "lblpromedio";
+            this.lblpromedio.Size = new System.Drawing.Size(83, 20);
+            this.lblpromedio.TabIndex = 13;
+            this.lblpromedio.Text = "promedio";
+            this.lblpromedio.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(591, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Visible = false;
+            // 
+            // lblmensaje
+            // 
+            this.lblmensaje.AutoSize = true;
+            this.lblmensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmensaje.Location = new System.Drawing.Point(591, 221);
+            this.lblmensaje.Name = "lblmensaje";
+            this.lblmensaje.Size = new System.Drawing.Size(76, 20);
+            this.lblmensaje.TabIndex = 15;
+            this.lblmensaje.Text = "mensaje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 470);
+            this.Controls.Add(this.lblmensaje);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblpromedio);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtNota5);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.TextBox txtNota5;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Label lblpromedio;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblmensaje;
+    }
+}
+
---
+++ taller 2 solucion 2/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taller_2_solucion_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double nota1, nota2, nota3, nota4, nota5, promedio;
+
+            nota1 = double.Parse(txtNota1.Text);
+
+            nota2 = double.Parse(txtNota2.Text);
+
+            nota3 = double.Parse(txtNota3.Text);
+
+
+            nota4 = double.Parse(txtNota4.Text);
+
+            nota5 = double.Parse(txtNota5.Text);
+
+            promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+
+            lblpromedio.Text = Convert.ToString(promedio);
+            
+
+
+
+            if (promedio >= 0 && promedio < 1)
+            {
+                lblmensaje.Text = "reprueba la materia sin lograr realisar proceso de recuperacion ";
+            }
+
+            else if (promedio >= 1.1 && promedio < 2.9)
+            {
+
+                lblmensaje.Text = "reprueba la materia y puede hacer habilitacion ";
+               
+            }
+
+            else if (promedio >= 3 && promedio < 3.9) 
+            {
+                lblmensaje.Text = "aprueba la materia con plan de mejora";
+            }
+
+            else if (promedio >= 4 && promedio < 4.5) 
+            {
+                lblmensaje.Text = "buen rendimiento";
+            }
+            else if (promedio >= 4.6 && promedio < 4.9) 
+            {
+                lblmensaje.Text = "exelente";
+            }
+
+            else if (promedio == 5) 
+            {
+                lblmensaje.Text = "se gradua con honores";
+            }
+            
+
+           lblpromedio.Visible = true;
+
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            lblpromedio.Text = String.Empty;
+            lblpromedio.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
